a54b860af654886bd82d80d25b7bfe05d9c845aa
rahul1990bhatia <rahul1990bhatia@gmail.com>
2024-02-21 01:05:51 +0000

    adding load functions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import yaml
 import ollama
 import argparse
+import bs4
 from pydantic import BaseModel
 from typing import Dict, Optional, List
 from langchain_community.document_loaders.wikipedia import WikipediaLoader
+from langchain_community.document_loaders.html_bs import BSHTMLLoader
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
 
 
 class ConfigureableLLM(BaseModel):
@@ -33,11 +39,49 @@ class ConfigureableLLM(BaseModel):
 
     def load_wiki(self) -> list:
         """This function loads user data provided in different formats"""
+        documents = []
         for pages in self.configuration["DataSources"]["wiki"]:
             print("Loading Wiki Page: ", pages)
             wiki = WikipediaLoader(query=pages, load_max_docs=10)
-            wiki.load()
-            print(wiki)
+            wiki_content = wiki.load()
+            documents.append(wiki_content)
+        return documents
+
+    def load_html(self) -> list:
+        """This functions loads user data from html pages"""
+        documents = []
+        for pages in self.configuration["DataSources"]["html"]:
+            print("Loading HTML Page: ", pages)
+            html = BSHTMLLoader(file_path=pages, open_encoding="utf8")
+            html_content = html.load()
+            documents.append(html_content)
+        return documents
+
+    def load_webpage(self) -> List:
+        """This functions loads user data from webpages"""
+        documents = []
+        for pages in self.configuration["DataSources"]["webpage"]:
+            print("Loading WebPage: ", pages)
+            loader = WebBaseLoader(pages)
+            data = loader.load()
+            documents.append(data)
+        return documents
+
+    def load_database(self) -> None:
+        """this function will load all data to chromadb"""
+        Documents = []
+        Documents += my_llm.load_wiki()
+        Documents += my_llm.load_html()
+        Documents += my_llm.load_webpage()
+        print("Total number of documents loaded: ", len(Documents))
+        text_splitter = RecursiveCharacterTextSplitter()
+        ollama_emb = OllamaEmbeddings(model=self.configuration["LLM"])
+        vectorstore = Chroma("LLM_Store", ollama_emb)
+        for doc in Documents:
+            splitted_docs = text_splitter.split_documents(doc)
+            vectorstore.add_documents(doc)
+        vectorstore.persist()
+        print("Data successfully loaded to chromaDB")
 
     def rag_chain(self, question: str) -> str:
         if self.configuration["RAG"] == "Enable":
@@ -55,5 +99,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     my_llm = ConfigureableLLM(**{"config_path": args.config})
     my_llm.load_config()
-    my_llm.load_wiki()
+    my_llm.load_database()
     # my_llm.rag_chain("What is Gita?")
